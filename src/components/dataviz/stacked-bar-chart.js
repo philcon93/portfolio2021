a70d8ce99ce112eb09c9b74bebe2da4fd1d80ae2
Phil <phil@neto.com.au>
2021-02-17 21:05:07 +1000

    made stacked barchart look pretty with chroma-js and d3-svg-legend

--- src/components/dataviz/stacked-bar-chart.js
+++ src/components/dataviz/stacked-bar-chart.js
@@ -3,6 +3,7 @@ import * as d3 from "d3";
 import chroma from "chroma-js";
 import PropTypes from 'prop-types';
 import { useD3 } from "d3blackbox";
+import legend from "d3-svg-legend";
 
 const VerticalAxis = ({scale, x, y}) => {
   const refAnchor = useD3(anchor => {
@@ -14,31 +15,43 @@ const VerticalAxis = ({scale, x, y}) => {
   return <g ref={refAnchor} transform={`translate(${x}, ${y})`} />;
 };
 
-const BarChart = ({ entries, yScale, width, marginLeft, color }) => {
+const BarChart = ({ entries, yScale, xScale, marginLeft, color }) => {
   return (
     <>
     { entries.map(([min, max], index) => (
       <rect
-        x={marginLeft + width(min)}
-        width={width(max) - width(min)}
+        x={marginLeft + xScale(min)}
+        width={xScale(max) - xScale(min)}
         y={yScale(yScale.domain()[index])}
         height={yScale.bandwidth()}
         key={yScale.domain()[index]}
         fill={color}>
-        <title>{min}, {max}</title>
+        <title>Age: {min} - {max}</title>
       </rect>
-  ))}
+    ))}
     </>
   )
 }
 
-const StackChart = ({ data, height }) => {
+const Legend = ({ scale, x, y }) => {
+  const refAnchor = useD3(anchor => {
+    const legendA = legend
+      .legendColor()
+      .scale(scale)
+      .title("Age group");
+
+    d3.select(anchor).call(legendA);
+  })
+  return <g ref={refAnchor} transform={`translate(${x}, ${y})`} />
+}
+
+const StackChart = ({ data, width, height }) => {
     const yScale = d3
       .scaleBand()
       .domain(data.map(d => d.category))
       .range([0, height]);
     
-    const x = 75;
+    const x = 215;
 
     const stack = d3
       .stack()
@@ -46,10 +59,17 @@ const StackChart = ({ data, height }) => {
 
     const stackData = stack(data);
 
-    const width = d3
+    const xScale = d3
       .scaleLinear()
       .domain([0, d3.max(stackData[2], d => d[1])])
-      .range([0, 400]);
+      .range([0, width/1.75]);
+
+    const color = chroma.brewer.pastel2;
+
+    const colorScale = d3
+      .scaleOrdinal()
+      .domain(["🧒 18 to 29 years", "🙍‍♂️ 30 to 59 years", "🧓 60 years or older"])
+      .range(color);
 
     return (
       <>
@@ -57,15 +77,15 @@ const StackChart = ({ data, height }) => {
       {
         stackData.map((entries, index) => (
           <BarChart
-            entries={entries}
-            yScale={yScale}
             key={index}
             marginLeft={x+2}
-            // color={color[index]}
-            width={width}
-          />
+            entries={entries}
+            yScale={yScale}
+            xScale={xScale}
+            color={color[index]} />
         ))
       }
+      <Legend scale={colorScale} x={width/1.75} y={height - 100} />
       </>
     );
   };
@@ -88,13 +108,27 @@ export const StackedChart = ({dataset, width, height}) => {
     return (
         <svg width={width} height={height}>
         {data && (
-          <StackChart data={data} height={height} />
+          <StackChart data={data} width={width} height={height} />
         )}
       </svg>
     );
 };
 
 VerticalAxis.propTypes = {
+  scale: PropTypes.func,
+  x: PropTypes.number,
+  y: PropTypes.number
+};
+
+BarChart.propTypes = {
+  entries: PropTypes.array,
+  yScale: PropTypes.func,
+  xScale: PropTypes.func,
+  marginLeft: PropTypes.number,
+  color: PropTypes.string
+};
+
+Legend.propTypes = {
   scale: PropTypes.func,
   x: PropTypes.number,
   y: PropTypes.number
