773575bc93774bfabe2870ac4cf1b971496d5c45
Phil <phil@neto.com.au>
2021-02-22 20:34:53 +1000

    completed carols word cloud

--- src/components/dataviz/carols-word-cloud.js
+++ src/components/dataviz/carols-word-cloud.js
@@ -2,28 +2,45 @@ import React, { useEffect, useState } from "react";
 import * as d3 from "d3";
 import PropTypes from 'prop-types';
 import { parseText } from '../../utilities/parse-text';
+import { WordCloud } from './word-cloud';
 
 export const CarolsWordCloud = ({ dataset, height, width }) => {
+  const catchAllGroup = "All carols";
   const [ data, setData ] = useState(null);
-  const [chosenCarol, setChosenCarol] = useState("All carols");
+  const [chosenCarol, setChosenCarol] = useState(catchAllGroup);
 
   useEffect(() => {
     d3.text(dataset)
-      .then(data => parseText(data, "All carols"))
+      .then(data => parseText(data, catchAllGroup))
       .then(data => setData(data));
   }, []);
+
+  const getWords = () => {
+    console.log(chosenCarol);
+    if (data === null) {
+      return [];
+    }
+    return data[chosenCarol]
+      .join()
+      .toLowerCase()
+      .split(/\W/)
+      .filter(word => word.trim().length > 0);
+  };
 
   return (
     data ? (
         <>
-            <select onChange={(e) => setChosenCarol(e.value)} value={chosenCarol}>
+            <select onChange={(e) => setChosenCarol(e.target.value)} value={chosenCarol} className="block w-full pl-3 pr-10 py-2 border rounded shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed text-black placeholder-gray-400 bg-white border-gray-300 focus:border-blue-500">
               {Object.keys(data).map((carol, index) => (
-                  <option key={index}>{carol}</option>
+                  <option key={index} value={carol}>{carol}</option>
               ))}
             </select>
-            <svg
-                width={width}
-                height={height}>
+            <svg width={width} height={height}>
+              <WordCloud
+                words={getWords()}
+                forCarol={chosenCarol}
+                width={width - 200}
+                height={height - 200} />
             </svg>
         </>
     ) : null
--- src/components/dataviz/word-cloud.js
+++ src/components/dataviz/word-cloud.js
@@ -1,17 +1,106 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import * as d3 from "d3";
 import PropTypes from 'prop-types';
-import { parseText } from '../../utilities/parse-text';
+import d3Cloud from 'd3-cloud';
+import chroma from "chroma-js";
 
-export const WordCloud = ({ dataset, height, width }) => {
+const count = (words) => {
+    let counts = {};
 
-  return (
-    null
-  )
+    for (let w in words) {
+      counts[words[w]] = (counts[words[w]] || 0) + 1;
+    }
+
+    return counts;
 }
 
+const createCloud = ({ words, width, height }) => {
+    return new Promise(resolve => {
+      const counts = count(words);
+  
+      const fontSize = d3
+        .scaleLog()
+        .domain(d3.extent(Object.values(counts)))
+        .range([5, 75]);
+  
+      const layout = d3Cloud()
+        .size([width, height])
+        .words(
+          Object.keys(counts)
+            .filter(w => counts[w] > 1)
+            .map(word => ({ word }))
+        )
+        .padding(5)
+        // .font("Impact")
+        .fontSize(d => fontSize(counts[d.word]))
+        .text(d => d.word)
+        .on("end", resolve);
+  
+      layout.start();
+    });
+};
+
+const Word = ({ style, children, ...props })  =>{
+    const ref = useRef();
+    const [ transform, setTransform ] = useState(props.transform);
+
+    useEffect(() => {
+        d3.select(ref.current)
+        .transition()
+        .duration(500)
+        .attr("transform", props.transform)
+        .on("end", () => setTransform(transform));
+    }, [ props ])
+  
+      return (
+        <text
+          transform={transform}
+          textAnchor="middle"
+          style={style}
+          ref={ref}>
+          {children}
+        </text>
+      );
+};
+
+export const WordCloud = ({ forCarol, words, height, width }) => {
+    const colors = chroma.brewer.dark2;
+    const [cloud, setCloud] = useState(null);
+
+    useEffect(() => {
+        createCloud({ words, width, height })
+            .then(setCloud);
+    }, [forCarol, width, height]);
+
+    return (
+        cloud &&
+        <g transform={`translate(${width / 2}, ${height / 2})`}>
+            {
+                cloud.map((w, index) => (
+                    <Word
+                        transform={`translate(${w.x}, ${w.y}) rotate(${w.rotate})`}
+                        style={{
+                            fontSize: w.size,
+                            fill: colors[index % colors.length]
+                        }}
+                        key={w.word}>
+                    {w.word}
+                  </Word>
+                ))
+            }
+        </g>
+    )
+}
+
+Word.propTypes = {
+    children: PropTypes.string,
+    style: PropTypes.object,
+    transform: PropTypes.string
+};
+
 WordCloud.propTypes = {
-  dataset: PropTypes.string,
-  height: PropTypes.number,
-  width: PropTypes.number
+    forCarol: PropTypes.string,
+    words: PropTypes.array,
+    height: PropTypes.number,
+    width: PropTypes.number
 };
