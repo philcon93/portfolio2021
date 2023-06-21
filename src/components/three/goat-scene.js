45346098457924682c3365d78a4cd06ded1ab784
Phil <phil@neto.com.au>
2023-06-22 09:23:01 +1000

    updated react-three dependencies because of stage issue

--- src/components/three/goat-scene.js
+++ src/components/three/goat-scene.js
@@ -1,17 +1,21 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Stage } from '@react-three/drei';
-import Model from './goat';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Stage } from "@react-three/drei";
+import Model from "./goat";
 
 export default function Scene() {
-  
-    return (
-        <Canvas style={{ position: 'relative', height: '40vh', width: '100%'}} dpr={[1, 2]} shadows camera={{ fov: 45, position: [0, 1.5, 4.5] }}>
-            <Suspense fallback={null}>
-                <Stage adjustCamera={false} intensity={0.8}>
-                    <Model />
-                </Stage>
-            </Suspense>
-        </Canvas>
-    )
+  return (
+    <Canvas
+      style={{ position: "relative", height: "40vh", width: "100%" }}
+      dpr={[1, 2]}
+      shadows
+      camera={{ fov: 45, position: [0, 1.5, 4.5] }}
+    >
+      <Suspense fallback={null}>
+        <Stage adjustCamera={false} intensity={0.8}>
+          <Model />
+        </Stage>
+      </Suspense>
+    </Canvas>
+  );
 }
